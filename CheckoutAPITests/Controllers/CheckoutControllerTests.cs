bf965715b18363561f361e7e3c0e782727f2d892
YEV\YevLocal <yev.berezovsky@gmail.com>
2023-11-05 14:40:13 -0500

    implementing unit tests

--- CheckoutAPITests/Controllers/CheckoutControllerTests.cs
+++ CheckoutAPITests/Controllers/CheckoutControllerTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutAPI.Controllers;
 
-namespace CheckoutAPITests.Controllers
+namespace QuickBuyAPI.Controllers.Tests
 {
-    internal class CheckoutControllerTests
+    [TestClass()]
+    public class CheckoutControllerTests
     {
+        [TestMethod()]
+        public void CheckoutTest_ReturnsCorrectTotalPrice()
+        {
+            // Arrange
+            var logger = NullLogger<CheckoutController>.Instance;
+            var controller = new CheckoutController(logger);
+            var watchIds = new List<string> { "001", "002", "001", "004", "003" };
+
+            // Act
+            var result = controller.Checkout(watchIds) as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            var price = (decimal)result.Value.GetType().GetProperty("price").GetValue(result.Value, null);
+            Assert.AreEqual(360m, price, "Total price should be 360");
+        }
     }
 }
